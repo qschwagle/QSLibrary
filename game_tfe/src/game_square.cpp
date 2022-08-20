da2e0e97bfa441c39b20a1d023a1894643147190
Quinton Schwagle <qschwagle@gmail.com>
2022-08-20 12:15:01 -0400

    Added function to DrawText and uses the DrawText to display text centered in the game squares

--- game_tfe/src/game_square.cpp
+++ game_tfe/src/game_square.cpp
@@ -1,7 +1,13 @@
 #include "game_tfe/game_square.h"
+
+#include <string>
+#include <iostream>
+
 #include "game_tfe/text.h"
 
+
 static RVector<4> GREEN = { 0.0f, 1.0f, 0.0f, 1.0f};
+static RVector<4> BLACK = { 0.0f, 0.0f, 0.0f, 1.0f};
 
 GameSquare::GameSquare(int value) : mValue{value}
 {
@@ -24,4 +30,7 @@ void GameSquare::Draw(Geometry<9>& out, float width)
     }
     RVector<3> pos = { mPosition[0], mPosition[1], 0.0f };
     CreateRectangle3D(out, pos, GREEN, width, width);
+    RVector<3> mid_point = { mPosition[0] + width / 2.0f, mPosition[1] + width / 2.0f, 0.0f };
+    std::string val_str = std::to_string(mValue);
+    DrawText(out, nullptr, mid_point, BLACK, val_str, 14, 100, 100, TextAlignment::CENTER);
 }
