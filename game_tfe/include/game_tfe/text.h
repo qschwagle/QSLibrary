7d926159652f141c9adb8d61acbcc3f05474d885
Quinton Schwagle <qschwagle@gmail.com>
2022-08-21 19:45:06 -0400

    Refactored DrawText removing the DrawText that forced a width and height on the resultant box.

--- game_tfe/include/game_tfe/text.h
+++ game_tfe/include/game_tfe/text.h
@@ -7,22 +7,6 @@
 #include "linalg/rvector.h"
 #include "geometry/geometry.h"
 
-
-/**
- * Write the string texture to out buffer and generate the vertices mapping to height and width
- * \param out out geometry buffer
- * \param coordinate translation to coordinate 
- * \param color color of text
- * \param width width of rectangle
- * \param height height of rectangle
- * \param text text to render
- * \param pt font size
- * \param screen_width width of screen in pixels
- * \param screen_height height of screen in pixels
- */
-void DrawText(Geometry<9>& out, RVector<3> coordinate, RVector<4> color, float width, float height, std::string& text, unsigned int pt,
-        unsigned int screen_width, unsigned int screen_height);
-
 /**
  * Write the string texture to buffer, generates vertices based around text size, and optionally writes the size to dim_out
  * \param out geometry buffer
