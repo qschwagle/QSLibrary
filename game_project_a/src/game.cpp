9b21ee78c3ceab83431f33b413ed4f9cc92c1259
Quinton Schwagle <qschwagle@gmail.com>
2022-09-26 02:43:19 -0400

    Added glad ( OPENGL 4.1 ) and loaded glad, opengl and clear color window

--- game_project_a/src/game.cpp
+++ game_project_a/src/game.cpp
@@ -2,6 +2,7 @@
 
 #include <sstream>
 
+#include "glad/glad.h"
 #include "GLFW/glfw3.h"
 
 Game::Game() noexcept
@@ -24,11 +25,29 @@ bool Game::Init(int argc, char **argv)
     }
     mGLFWInitialized = true;
 
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
+
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
     if(!((mWindow = glfwCreateWindow(1280, 720, "Hello World", nullptr, nullptr)))) {
         auto msg = std::stringstream("Game::Init: Unable to create window. should terminate");
         mLogger->WriteCritical(msg);
         return false;
     }
+
+    glfwMakeContextCurrent(mWindow);
+
+    glfwSwapInterval(1);
+
+    if(!gladLoadGLLoader((GLADloadproc) glfwGetProcAddress)) {
+        auto msg = std::stringstream("Game::Init: failed to initialize glad. should terminate");
+        mLogger->WriteCritical(msg);
+        return false;
+    }
+
+    glViewport(0, 0, 1280, 720);
 
     return true;
 }
@@ -37,7 +56,12 @@ int Game::Run()
 {
     while(!glfwWindowShouldClose(mWindow)) {
 
-        glfwWaitEventsTimeout(0.100);
+        glClearColor(1.0, 0.0, 0.0, 1.0);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwSwapBuffers(mWindow);
+
+        glfwWaitEventsTimeout(0.001);
     }
     return 0;
 }
